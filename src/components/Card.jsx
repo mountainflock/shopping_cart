e9f2ff253cf43d939c4599f60e11ef21032fe565
Marina <118333236+mountainflock@users.noreply.github.com.>
2024-03-04 20:34:04 +0300

    Complete initial version of the app: main components, fetching, adding to cart logic and layout

---
+++ src/components/Card.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+import "./card.css";
+import ProductInput from "./ProductInput";
+
+const Card = ({ id, title, image, price, handleAddToCart }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleAddToCartButtonClick = (e) => {
+    e.preventDefault();
+    const newProduct = { id, title, image, price, quantity: quantity };
+    handleAddToCart({ ...newProduct, quantity: quantity });
+  };
+
+  const handleIncreaseButtonClick = (e) => {
+    e.preventDefault();
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecreaseButtonClick = (e) => {
+    e.preventDefault();
+    if (quantity === 0) return;
+    setQuantity(quantity - 1);
+  };
+
+  const handleQuantityChange = (e) => {
+    setQuantity(parseInt(e.target.value));
+  };
+
+  return (
+    <div className="card">
+      <div className="image-div">
+        <img src={image} alt="product image" className="card-image" />
+      </div>
+      <div className="card-title">{title}</div>
+      <div className="price-div">${price}</div>
+      <div className="input-plus-button">
+        <ProductInput
+          id={id}
+          quantity={quantity}
+          handleIncreaseButtonClick={handleIncreaseButtonClick}
+          handleDecreaseButtonClick={handleDecreaseButtonClick}
+          handleQuantityChange={handleQuantityChange}
+        />
+        <div>
+          <button
+            onClick={handleAddToCartButtonClick}
+            className="add-to-cart-button"
+          >
+            ADD TO CART
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
