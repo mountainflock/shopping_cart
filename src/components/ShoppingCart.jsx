96dd60d6abf14c7a229e85a0c0455e0fbf94e4f4
Marina <reddymikks@gmail.noreply.github.com>
2024-03-12 20:56:51 +0300

    Restore deleting product from a shopping cart UI logic

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -4,13 +4,13 @@ import ShoppingCartItem from "./ShoppingCartItem";
 
 const ShoppingCart = () => {
   const { shoppingCart, clearShoppingCart } = useContext(ShopContext);
-  console.log(shoppingCart);
   return (
     <div className="shopping-cart">
       <div className="shopping-cart-title">Shopping Cart</div>
       {shoppingCart.map((product, i) => (
         <ShoppingCartItem
           key={i}
+          id={product.productId.id}
           title={product.productId.title}
           quantity={product.productId.quantity}
           price={product.productId.price}
@@ -20,7 +20,7 @@ const ShoppingCart = () => {
       <div className="total-price-div">
         Total: ${""}
         {shoppingCart.reduce((sum, value) => {
-          return sum + value.productId.price;
+          return sum + value.productId.price * value.productId.quantity;
         }, 0)}
       </div>
       <button className="delete-all-button" onClick={clearShoppingCart}>
